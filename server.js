d844b5cc50c88cdd0c960f4cfdac910a6ecf6984
Lalitha Soumya Ayyagari <soumyaayyagari2310@gmail.com>
2021-05-03 12:29:30 +0530

    Changes made to Resume Page

--- server.js
+++ server.js
@@ -4,19 +4,24 @@ const app = express();
 
 app.use(express.static(__dirname+"/frontend"));
  
-app.get("/", function(req, res){
+/*app.get("/", function(req, res){
     res.send("Welcome to Lalitha's Basic Site");
-})
+});*/
  
 // Heroku will automatically set an environment variable called PORT
 const PORT = process.env.PORT || 3000;
+
+app.get("/", function(req, res){
+    let i = __dirname + "/frontend/html/home.html";
+    res.sendFile(i);
+});
  
 app.get("/resume", function(req, res){
-    let i=__dirname+"/frontend/html/resume.html"
+    let i=__dirname+"/frontend/html/resume.html";
     res.sendFile(i);
 
 });
 // Start the server
 app.listen(PORT, function(){
     console.log("Server Starting running on http://localhost:"+PORT);
-})
+});
