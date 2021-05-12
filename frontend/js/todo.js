4e100d6c9fd164cc80c72f00cb9c5e0585c22365
Lalitha Soumya Ayyagari <soumyaayyagari2310@gmail.com>
2021-05-13 00:23:15 +0530

    changes made to todo list

--- frontend/js/todo.js
+++ frontend/js/todo.js
@@ -6,6 +6,8 @@ function newTask(){
     var newCol1 = document.createElement("span")
     var newCol2 = document.createElement("span")
     var newCol3 = document.createElement("span")
+    var newLine = document.createElement("br")
+    var newLine1 = document.createElement("br")
 
     newCol1.innerHTML = gt
     newCol2.innerHTML = "<i class='fas fa-check-circle fa-lg'></i>"
@@ -14,6 +16,8 @@ function newTask(){
     newRow.classList.add("field")
     newCol2.classList.add("c1")
     newCol3.classList.add("c2")
+    newCol1.classList.add("matter")
+    //newLine.classList.add("hline")
 
    /* newCol2.onclick(function(){
         var c = 0
@@ -25,8 +29,10 @@ function newTask(){
     })*/
 
     newRow.appendChild(newCol1)
-    newRow.appendChild(newCol2)
     newRow.appendChild(newCol3)
+    newRow.appendChild(newCol2)
+    newRow.appendChild(newLine)
+    newRow.appendChild(newLine1)
     
 
 
